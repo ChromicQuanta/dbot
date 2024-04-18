347782af90f0e63804388f9f061eb5cb89879514
ChromicQuanta <59491666+ChromicQuanta@users.noreply.github.com>
2024-04-18 11:03:40 +1000

    Update dbot.js

--- dbot.js
+++ dbot.js
@@ -1,6 +1,6 @@
 const {Client, Intents} = require("discord.js")
 //const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
-const client = new Client({})
+const client = new Client({intents: []})
 console.log("e")
 client.on('ready', ()=>{
   console.log(client.user.tag)
